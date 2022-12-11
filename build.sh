dc09c6786ff785100ef1151d90f0f349b336d547
Nebula <me@itsnebula.net>
2022-12-10 21:09:31 -0500

    signing is back

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+GPG_KEY="FB04F6C8EC56DA32F33008C53D1B28A5FACCB53B"
 OUTPUT_DIR="publish"
 
 script_full_path=$(dirname "$0")
@@ -6,7 +7,7 @@ cd "$script_full_path" || exit 1
 rm -rf $OUTPUT_DIR
 mkdir -p $OUTPUT_DIR
 
-echo "[Repository] Generating Packages..."
+echo "[*] Generating Packages..."
 apt-ftparchive packages ./pool > $OUTPUT_DIR/Packages
 zstd -q -c19 $OUTPUT_DIR/Packages > $OUTPUT_DIR/Packages.zst
 xz -c9 $OUTPUT_DIR/Packages > $OUTPUT_DIR/Packages.xz
@@ -15,7 +16,7 @@ gzip -nc9 $OUTPUT_DIR/Packages > $OUTPUT_DIR/Packages.gz
 lzma -c9 $OUTPUT_DIR/Packages > $OUTPUT_DIR/Packages.lzma
 lz4 -c9 $OUTPUT_DIR/Packages > $OUTPUT_DIR/Packages.lz4
 
-echo "[Repository] Generating Release..."
+echo "[*] Generating Release..."
 apt-ftparchive \
     -o APT::FTPArchive::Release::Origin="palera1n" \
     -o APT::FTPArchive::Release::Label="palera1n" \
@@ -24,9 +25,16 @@ apt-ftparchive \
     -o APT::FTPArchive::Release::Codename="palera1n-repo" \
     -o APT::FTPArchive::Release::Architectures="iphoneos-arm64" \
     -o APT::FTPArchive::Release::Components="main" \
-    -o APT::FTPArchive::Release::Description="palera1n's official repo." \
+    -o APT::FTPArchive::Release::Description="palera1n's official repo" \
     release $OUTPUT_DIR > $OUTPUT_DIR/Release
 
+echo "[*] Signing Release using GPG Key..."
+gpg -abs -u $GPG_KEY -o $OUTPUT_DIR/Release.gpg $OUTPUT_DIR/Release
+gpg -abs -u $GPG_KEY --clearsign -o $OUTPUT_DIR/InRelease $OUTPUT_DIR/Release
+
+echo "[*] Copying files..."
 cp -R pool "$OUTPUT_DIR"
 cp *.png "$OUTPUT_DIR"
 cp index.html "$OUTPUT_DIR"
+
+echo "[*] Done!"
