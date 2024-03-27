20305a1422e41fef93f3dce2ec0e9415b554a803
Nick Chan <towinchenmi@gmail.com>
2024-03-28 00:57:13 +0800

    cleanup repo generation

--- build.sh
+++ build.sh
@@ -1,83 +1,49 @@
 #!/bin/bash
 GPG_KEY="F3594A65343002C55E35FB1E9E7E89B50CCF8DAA"
 OUTPUT_DIR="publish"
+ARCHS="iphoneos-arm iphoneos-arm64 appletvos-arm64"
 
 script_full_path=$(dirname "$0")
 cd "$script_full_path" || exit 1
 rm -rf $OUTPUT_DIR
-mkdir -p $OUTPUT_DIR/{rootful,rootless,appletv-rootful}
-
-dirs=(./pool ./pool/iphoneos-arm ./pool/iphoneos-arm64 ./pool/appletvos-arm64)
-
-set_arch_vars() {
-    case $(basename "$1") in
-        pool)
-            output_dir=$OUTPUT_DIR
-            extra=(extra_packages*)
-            ;;
-        iphoneos-arm)
-            arch=iphoneos-arm
-            output_dir=$OUTPUT_DIR/rootful
-            extra=(extra_packages_rootful)
-            ;;
-        iphoneos-arm64)
-            arch=iphoneos-arm64
-            output_dir=$OUTPUT_DIR/rootless
-            extra=(extra_packages_rootless)
-            ;;
-        appletvos-arm64)
-            arch=appletvos-arm64
-            output_dir=$OUTPUT_DIR/appletv-rootful
-            extra=(extra_packages_appletv_rootful)
-            ;;
-    esac
-}
-
+mkdir $OUTPUT_DIR
 
 echo "[*] Generating Packages..."
-for d in "${dirs[@]}"; do
-    set_arch_vars "$d"
-    apt-ftparchive packages "$d" > $output_dir/Packages
-    apt-ftparchive contents "$d" > $output_dir/Contents-"${arch}"
-    echo >> $output_dir/Packages
-    cat "${extra[@]}" >> $output_dir/Packages 2>/dev/null
 
-    zstd -q -c19 $output_dir/Contents-"${arch}" > $output_dir/Contents-"${arch}".zst
-    xz -c9 $output_dir/Contents-"${arch}" > $output_dir/Contents-"${arch}".xz
-    bzip2 -c9 $output_dir/Contents-"${arch}" > $output_dir/Contents-"${arch}".bz2
-    gzip -nc9 $output_dir/Contents-"${arch}" > $output_dir/Contents-"${arch}".gz
-    lzma -c9 $output_dir/Contents-"${arch}" > $output_dir/Contents-"${arch}".lzma
-    lz4 -c9 $output_dir/Contents-"${arch}" > $output_dir/Contents-"${arch}".lz4
+apt-ftparchive packages ./pool > $OUTPUT_DIR/Packages
+zstd -q -c19 $OUTPUT_DIR/Packages > $OUTPUT_DIR/Packages.zst
+xz -c9 $OUTPUT_DIR/Packages > $OUTPUT_DIR/Packages.xz
+bzip2 -c9 $OUTPUT_DIR/Packages > $OUTPUT_DIR/Packages.bz2
+gzip -nc9 $OUTPUT_DIR/Packages > $OUTPUT_DIR/Packages.gz
+lzma -c9 $OUTPUT_DIR/Packages > $OUTPUT_DIR/Packages.lzma
+lz4 -c9 $OUTPUT_DIR/Packages > $OUTPUT_DIR/Packages.lz4
 
-    zstd -q -c19 $output_dir/Packages > $output_dir/Packages.zst
-    xz -c9 $output_dir/Packages > $output_dir/Packages.xz
-    bzip2 -c9 $output_dir/Packages > $output_dir/Packages.bz2
-    gzip -nc9 $output_dir/Packages > $output_dir/Packages.gz
-    lzma -c9 $output_dir/Packages > $output_dir/Packages.lzma
-    lz4 -c9 $output_dir/Packages > $output_dir/Packages.lz4
+echo "[*] Generating Contents"
+for arch in ${ARCHS}; do
+    apt-ftparchive contents ./pool/"${arch}" > $OUTPUT_DIR/Contents-"${arch}"
+    zstd -q -c19 $OUTPUT_DIR/Contents-"${arch}" > $OUTPUT_DIR/Contents-"${arch}".zst
+    xz -c9 $OUTPUT_DIR/Contents-"${arch}" > $OUTPUT_DIR/Contents-"${arch}".xz
+    bzip2 -c9 $OUTPUT_DIR/Contents-"${arch}" > $OUTPUT_DIR/Contents-"${arch}".bz2
+    gzip -nc9 $OUTPUT_DIR/Contents-"${arch}" > $OUTPUT_DIR/Contents-"${arch}".gz
+    lzma -c9 $OUTPUT_DIR/Contents-"${arch}" > $OUTPUT_DIR/Contents-"${arch}".lzma
+    lz4 -c9 $OUTPUT_DIR/Contents-"${arch}" > $OUTPUT_DIR/Contents-"${arch}".lz4
 done
 
 echo "[*] Generating Release..."
-for d in "${dirs[@]}"; do
-    set_arch_vars "$d"
-    apt-ftparchive \
-        -o APT::FTPArchive::Release::Origin="palera1n" \
-        -o APT::FTPArchive::Release::Label="palera1n" \
-        -o APT::FTPArchive::Release::Suite="stable" \
-        -o APT::FTPArchive::Release::Version="1.0" \
-        -o APT::FTPArchive::Release::Codename="palera1n-repo" \
-        -o APT::FTPArchive::Release::Architectures="iphoneos-arm iphoneos-arm64 appletvos-arm64" \
-        -o APT::FTPArchive::Release::Components="main" \
-        -o APT::FTPArchive::Release::Description="palera1n's official repo" \
-        release $output_dir > $output_dir/Release
-done
+apt-ftparchive \
+    -o APT::FTPArchive::Release::Origin="palera1n" \
+    -o APT::FTPArchive::Release::Label="palera1n" \
+    -o APT::FTPArchive::Release::Suite="stable" \
+    -o APT::FTPArchive::Release::Version="1.0" \
+    -o APT::FTPArchive::Release::Codename="palera1n-repo" \
+    -o APT::FTPArchive::Release::Architectures="iphoneos-arm iphoneos-arm64 appletvos-arm64" \
+    -o APT::FTPArchive::Release::Components="main" \
+    -o APT::FTPArchive::Release::Description="palera1n's official repo" \
+    release $OUTPUT_DIR > $OUTPUT_DIR/Release
 
 echo "[*] Signing Release using GPG Key..."
-for d in "${dirs[@]}"; do
-    set_arch_vars "$d"
-    gpg -abs -u $GPG_KEY -o $output_dir/Release.gpg $output_dir/Release
-    gpg -abs -u $GPG_KEY --clearsign -o $output_dir/InRelease $output_dir/Release
-done
+gpg -abs -u $GPG_KEY -o $OUTPUT_DIR/Release.gpg $OUTPUT_DIR/Release
+gpg -abs -u $GPG_KEY --clearsign -o $OUTPUT_DIR/InRelease $OUTPUT_DIR/Release
 
 echo "[*] Copying files..."
 cp -R pool "$OUTPUT_DIR"
